6873f0ceadcfb00be612678f76d1f67276353f3f
betriz-sanchez-flores <beatrizsanchezflores2000@gmail.com>
2021-10-16 14:59:38 -0600

    CocheFactory

--- Fabrica_Abstracta/src/main/java/org/beatriz/AstraCarroceria.java
+++ Fabrica_Abstracta/src/main/java/org/beatriz/AstraCarroceria.java
@@ -1,1 +1,12 @@
 package org.beatriz;
+public class AstraCarroceria extends Carroceria
+{
+
+    @Override
+    public String lacarroceria()
+    {
+        String carrocerosia="Opel carroceria= Color: pinkish, Chasis: independiente, Carroceria: Autoportante";
+        return carrocerosia;
+    }
+
+}
