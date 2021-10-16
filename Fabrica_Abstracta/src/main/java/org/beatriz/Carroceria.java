65f55a6fb473cf8b4e61d91c5db1822a9bf567b3
betriz-sanchez-flores <beatrizsanchezflores2000@gmail.com>
2021-10-16 15:07:01 -0600

    clase carroceria

---
+++ Fabrica_Abstracta/src/main/java/org/beatriz/Carroceria.java
@@ -0,0 +1,17 @@
+package org.beatriz;
+
+public abstract class Carroceria {
+
+    private String Carroceria;
+
+    public abstract String lacarroceria();
+
+    public String getCarroceria(){
+        return Carroceria;
+    }
+
+    public void setCarroceria(String Carroceria){
+        this.Carroceria=Carroceria;
+    }
+
+}
